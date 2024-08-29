e8a0cc1334018a79fd4f2e4e6f02074f4818c9e3
Alelizzt <alejandrolistc@hotmail.com>
2024-08-29 15:39:19 -0500

    Add endpoints to login and signup blog users

--- blog-system/src/main/java/com/system/blog/controller/AuthController.java
+++ blog-system/src/main/java/com/system/blog/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.system.blog.controller;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,12 +9,18 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.system.blog.dto.LoginDto;
+import com.system.blog.dto.RegisterDto;
+import com.system.blog.model.BlogUser;
+import com.system.blog.model.Role;
+import com.system.blog.repository.BlogUserRepository;
+import com.system.blog.repository.RoleRepository;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -21,6 +29,15 @@ public class AuthController {
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private BlogUserRepository blogUserRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@PostMapping("/login")
 	public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto) {
 		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(), loginDto.getPassword()));
@@ -28,5 +45,28 @@ public class AuthController {
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 		
 		return new ResponseEntity<>("Login successful!", HttpStatus.OK);
+	}
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> userSignUp(@RequestBody RegisterDto registerDto) {
+		if(blogUserRepository.existsByUsername(registerDto.getUsername())) {
+			return new ResponseEntity<>("Already registered user with that username!", HttpStatus.BAD_REQUEST);
+		}
+		if(blogUserRepository.existsByEmail(registerDto.getEmail())) {
+			return new ResponseEntity<>("Already registered user with that email!", HttpStatus.BAD_REQUEST);
+		}
+		
+		BlogUser user = new BlogUser();
+		user.setName(registerDto.getName());
+		user.setUsername(registerDto.getUsername());
+		user.setEmail(registerDto.getEmail());
+		user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+		
+		Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+		user.setRoles(Collections.singleton(roles));
+		
+		blogUserRepository.save(user);
+		return new ResponseEntity<>("Succesfully registered user!", HttpStatus.OK);
+		
 	}
 }
--- blog-system/src/main/java/com/system/blog/repository/RoleRepository.java
+++ blog-system/src/main/java/com/system/blog/repository/RoleRepository.java
@@ -5,9 +5,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.system.blog.model.Role;
-import com.system.blog.model.BlogUser;
 
-public interface RoleRepository extends JpaRepository<BlogUser, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
 	public Optional<Role> findByName(String name);
 }
